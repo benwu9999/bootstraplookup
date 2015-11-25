b1285db8580c7958f6dc7edf90fd6221eaa07cca
ben wu <benwu9999@gmail.com>
2015-11-24 22:31:35 -0500

    added basic jquery autocomplete

--- bootstraplookup.py
+++ bootstraplookup.py
@@ -1,15 +1,28 @@
 import cherrypy
+import tinycss
+from tinycss.css21 import RuleSet
+import urllib.request
+
+global parsed_bootstrap
       
 class BootstrapLookup(object):
     exposed = True
     
-    def _cp_dispatch(self, vpath):
-        print(vpath)
-    
     @cherrypy.expose
     @cherrypy.tools.accept(media='text/plain')
-    def GET(self):
-        return """{"suggestions": ["United Arab Emirates", "United States"]}"""
+    def GET(self, var=None, **params):
+        global parsed_bootstrap
+        splitter = cherrypy.request.config['bootstrap.splitter']
+        cherrypy.log("{}.GET invoked with {}".format(type(self).__name__, str(params)))
+        rule_names = []
+        for rule in parsed_bootstrap.rules:
+            if type(rule) is RuleSet:
+                css_names = rule.selector.as_css()
+                rule_names.extend(css_names.split(splitter))
+        rep = '","'.join(rule_names)
+        rep = '["'+rep+'"]'
+        return rep
+#         return str(rule_names)
 
 class StaticPage(object):
     exposed = True
@@ -17,7 +30,13 @@ class StaticPage(object):
 #     def index(self):
 #         pass
 
-#cherrypy.quickstart(BootstrapLookup(), "/", "bootstrapLookup.config")
+def parse_bootstrap(bootstrap_url):
+    parser = tinycss.make_parser('page3')
+    global parsed_bootstrap
+    with urllib.request.urlopen(bootstrap_url) as url: 
+        parsed_bootstrap = parser.parse_stylesheet_bytes(url.read())
+        cherrypy.log("parsed bootstrap {} from {}".format(bootstrap_url, str(parsed_bootstrap)))
+
 if __name__ == '__main__':
 
     cherrypy.tree.mount(
@@ -25,10 +44,14 @@ if __name__ == '__main__':
         "bootstraplookup.config"
     )
     
-    cherrypy.tree.mount(
+    app = cherrypy.tree.mount(
         StaticPage(), '/',
         "bootstraplookup.config"
     )
+    
+    # TODO write a tutorial on lambda
+    # TODO dig why bootstrap.url returns error message in log
+    cherrypy.engine.subscribe('start', lambda: parse_bootstrap(app.config['/']['bootstrap.url']))
     
     cherrypy.engine.start()
     cherrypy.engine.block()
