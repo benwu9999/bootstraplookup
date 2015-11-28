4c857774691f5f37af6be63ff9e38afb73f74b96
ben wu <benwu9999@gmail.com>
2015-11-28 17:17:31 -0500

    '

--- bootstraplookup.py
+++ bootstraplookup.py
@@ -11,24 +11,31 @@ class BootstrapLookup(object):
     @cherrypy.expose
     @cherrypy.tools.accept(media='text/plain')
     def GET(self, var=None, **params):
-        global parsed_bootstrap
-        splitter = cherrypy.request.config['bootstrap.splitter']
         cherrypy.log("{}.GET invoked with {}".format(type(self).__name__, str(params)))
+        
+        global parsed_bootstrap
+        splitter = cherrypy.request.config['bootstrap_splitter']
+        term_name = cherrypy.request.config['bootstrap_term_name']
+        term = params.get(term_name);
+        
         rule_names = []
         for rule in parsed_bootstrap.rules:
             if type(rule) is RuleSet:
                 css_names = rule.selector.as_css()
-                rule_names.extend(css_names.split(splitter))
+                css_names_list = css_names.split(splitter)
+                css_names_list = [name for name in css_names_list if term in name]
+                rule_names.extend(css_names_list)
         rep = '","'.join(rule_names)
         rep = '["'+rep+'"]'
         return rep
 #         return str(rule_names)
+#         return """["Boston Celtics", "Chicago Bulls", "Miami Heat", "Orlando Magic", "Atlanta Hawks", "Philadelphia Sixers", "New York Knicks", "Indiana Pacers", "Charlotte Bobcats", "Milwaukee Bucks", "Detroit Pistons", "New Jersey Nets", "Toronto Raptors", "Washington Wizards", "Cleveland Cavaliers"]"""
 
 class StaticPage(object):
     exposed = True
-#     @cherrypy.expose
-#     def index(self):
-#         pass
+    @cherrypy.expose
+    def index(self):
+        pass
 
 def parse_bootstrap(bootstrap_url):
     parser = tinycss.make_parser('page3')
@@ -51,7 +58,7 @@ if __name__ == '__main__':
     
     # TODO write a tutorial on lambda
     # TODO dig why bootstrap.url returns error message in log
-    cherrypy.engine.subscribe('start', lambda: parse_bootstrap(app.config['/']['bootstrap.url']))
+    cherrypy.engine.subscribe('start', lambda: parse_bootstrap(app.config['/']['bootstrap_url']))
     
     cherrypy.engine.start()
     cherrypy.engine.block()
