b63fffc90a7e70a2634db6f40aab5c46ac126960
ben wu <benwu9999@gmail.com>
2015-12-09 20:35:11 -0500

    comit

--- bootstraplookup.py
+++ bootstraplookup.py
@@ -1,9 +1,11 @@
+import json
 import cherrypy
 import tinycss
 from tinycss.css21 import RuleSet
 import urllib.request
 
 global parsed_bootstrap
+global parsed_bootstrap_dict
       
 class BootstrapLookup(object):
     exposed = True
@@ -28,9 +30,33 @@ class BootstrapLookup(object):
         rep = '","'.join(rule_names)
         rep = '["'+rep+'"]'
         return rep
-#         return str(rule_names)
-#         return """["Boston Celtics", "Chicago Bulls", "Miami Heat", "Orlando Magic", "Atlanta Hawks", "Philadelphia Sixers", "New York Knicks", "Indiana Pacers", "Charlotte Bobcats", "Milwaukee Bucks", "Detroit Pistons", "New Jersey Nets", "Toronto Raptors", "Washington Wizards", "Cleveland Cavaliers"]"""
 
+class BootstrapDefLookup(object):
+    exposed = True
+    
+    @cherrypy.expose
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self, var=None, **params):
+        cherrypy.log("{}.GET invoked with {}".format(type(self).__name__, str(params)))
+        global parsed_bootstrap_dict
+        term = params.get(term_name);
+        definition = parsed_bootstrap_dict.get(term)
+        return definition
+        
+    def extractDef(self):
+        for rule in parsed_bootstrap.rules:
+            if type(rule) is RuleSet:
+                css_names = rule.selector.as_css()
+                css_names_list = css_names.split(splitter)
+                definition = ""
+                for declaration in rule.declarations:
+                    definition += declaration.name
+                    definition += ":"
+                    definition += ",".join(declaration.value)
+                    definition += "\\n"
+                for css_name in css_names_list:
+                    parsed_bootstrap_dict[css_name] = definition
+                    
 class StaticPage(object):
     exposed = True
     @cherrypy.expose
@@ -43,6 +69,13 @@ def parse_bootstrap(bootstrap_url):
     with urllib.request.urlopen(bootstrap_url) as url: 
         parsed_bootstrap = parser.parse_stylesheet_bytes(url.read())
         cherrypy.log("parsed bootstrap {} from {}".format(bootstrap_url, str(parsed_bootstrap)))
+        
+class TestLink(object):
+    exposed = True
+    @cherrypy.expose
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self, var=None, **params):
+        return "data:1,2\\nzelta:3,4"
 
 if __name__ == '__main__':
 
@@ -52,12 +85,21 @@ if __name__ == '__main__':
     )
     
     app = cherrypy.tree.mount(
-        StaticPage(), '/',
+        BootstrapDefLookup(), '/bootstrapdeflookup',
         "bootstraplookup.config"
     )
     
+#     app = cherrypy.tree.mount(
+#         StaticPage(), '/',
+#         "bootstraplookup.config"
+#     )
+#     
+#     app = cherrypy.tree.mount(
+#         TestLink(), '/testlink',
+#         "bootstraplookup.config"
+#     )
+    
     # TODO write a tutorial on lambda
-    # TODO dig why bootstrap.url returns error message in log
     cherrypy.engine.subscribe('start', lambda: parse_bootstrap(app.config['/']['bootstrap_url']))
     
     cherrypy.engine.start()
